33eb32d663bdadb1b77f2ab4b5f9a2ba042fe2b7
Dan Clawson <clawson@uen.org>
2015-03-24 17:48:33 -0600

    Add FizzBuzz loop.  (Step 4)

--- FizzBuzz/AppDelegate.m
+++ FizzBuzz/AppDelegate.m
@@ -16,7 +16,22 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    for (int x = 1; x < 101; x++) {
+        BOOL buzzed = [self isBuzzed:x];
+        BOOL fizzed = [self isFizzed:x];
+        
+        if (buzzed && !fizzed) {
+            NSLog(@"Buzzed");
+        } else if (fizzed && !buzzed) {
+            NSLog(@"Fizzed");
+        } else if (buzzed && fizzed) {
+            NSLog(@"FizzBuzz");
+        } else {
+            NSLog(@"%d is just %d", x, x);
+        }
+    }
+    
     return YES;
 }
 
@@ -42,4 +57,58 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL) isBuzzed: (NSInteger) input {
+    // Buzz Check:  If a number is a multiple of 3 or contains the digit 3 return true, otherwise return false
+    
+    // Convert the incoming integer to a string and determine if it has a 3
+    // There are a number of ways to convert a number to its string representation.  Here are two:
+
+    //NSString *inputAsString = [[NSNumber numberWithInteger:input] stringValue];
+    NSString *inputAsString = [@(input) stringValue];
+    
+    BOOL hasThree = FALSE;
+
+    for (long x = 0; x < inputAsString.length; x++) {
+        char sample = [inputAsString characterAtIndex:x];
+        if (sample == '3') {
+            hasThree = TRUE;
+            
+            x = (long) inputAsString.length;
+        }
+    }
+    
+    // Determine and return
+    if (input % 3 == 0) {
+        return TRUE;
+    } else if (hasThree) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+
+- (BOOL) isFizzed: (NSInteger) input {
+    
+    NSString *inputAsString = [[NSNumber numberWithInteger:input] stringValue];
+    
+    BOOL hasFive = FALSE;
+    
+    for (long x = 0; x < inputAsString.length; x++) {
+        char sample = [inputAsString characterAtIndex:x];
+        if (sample == '5') {
+            hasFive = TRUE;
+            
+            x = (long) inputAsString.length;
+        }
+    }
+    
+    if (input % 5 == 0) {
+        return TRUE;
+    } else if (hasFive) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
 @end
